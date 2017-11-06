e07c82b7353fd6f67dd55b40f28a05cb82dc6832
l-radtke <33435000+l-radtke@users.noreply.github.com>
2017-11-06 17:56:14 -0500

    Update Earthquakes group.py

--- Earthquakes group.py
+++ Earthquakes group.py
@@ -34,7 +34,7 @@ for line in QuakeFile:
     if 0 < depth and depth < 210:
         plt.scatter(longitude, latitude, label = "earthquakes")
         image = mpimg.imread ("world_map.pdf")
-        plt.imshow(image, extent=[-200,200,-60,80)
+        plt.imshow(image, extent=[-200,200,-60,80])
         plt.xlabel("Longitude")
         plt.ylabel("Latitude")
         plt.show()
